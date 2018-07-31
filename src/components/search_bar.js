c28ed760ab5a4bd560827c4c02d923ef81c954a5
Koki Nakamichi <kokinakamichi0617@g.ecc.u-tokyo.ac.jp>
2018-08-01 04:27:53 +0900

    Lesson ~20 create controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input /> ; // = React.createElement (vanilla JS)
-};
+// Lesson 15, functional component
+// const SearchBar = () => {
+//   return <input /> ; // = React.createElement (vanilla JS)
+// };
+
+class SearchBar extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {term: ''}; // term = search term つまり value of input. constructor = sets default state
+  }
+
+  render () {
+    // this.state.term = e.target.value // BAD!
+    return (
+      <div>
+        <input
+        value = {this.state.term}
+        onChange={e => this.setState({term: e.target.value})}
+        />
+      </div>
+    ) ;
+  }
+
+  onInputchange (e) {
+
+  }
+}
 
 export default SearchBar;
+
+// React, {Component} Component =
+// const Component = React.Component
